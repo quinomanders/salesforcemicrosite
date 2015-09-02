eae116018d8f785bc348206963bbafc5d437ea5f
Quino <quino.manders@gmail.com>
2015-09-02 10:50:43 +0200

    test2

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-  //$config['base_url'] = 'https://peaceful-beyond-6584.herokuapp.com/';
+  $config['base_url'] = 'https://peaceful-beyond-6584.herokuapp.com/';
   //$config['base_url'] = 'http://localhost/salesforce';
 
 /*
